048a9f4b9dcabcbeba123e56cbbcf2392721a1bd
suchithalingamaneni <142559138+suchithalingamaneni@users.noreply.github.com>
2024-12-08 06:53:06 +0530

    Add files via upload

---
+++ PromotehandloomsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.klu.promotehandlooms;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotehandloomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
